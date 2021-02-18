ba8fe48d975d91d963cd3dd6334d8ff1ba85d832
Pontus Alexander <pontus.alexander@gmail.com>
2021-02-18 19:16:12 +0100

    Allow state to be updated with callback
    Mimic setState(n => n + 1)

--- src/hooks/useSharedState.ts
+++ src/hooks/useSharedState.ts
@@ -4,12 +4,19 @@ import { Context } from "../context/SharedState";
 export function useSharedState<T>(
   key: string,
   fallbackValue: T
-): [T, (value: T) => void] {
+): [T, React.Dispatch<React.SetStateAction<T>>] {
   const [state, setState] = useContext(Context);
 
   const setValue = useCallback(
-    (value: T) => {
-      setState((values) => ({ ...values, [key]: value }));
+    (value: T | ((prevValue: T) => T)) => {
+      if (value instanceof Function) {
+        setState((values) => {
+          const prevValue = key in values ? values[key] : fallbackValue;
+          return { ...values, [key]: value(prevValue) };
+        });
+      } else {
+        setState((values) => ({ ...values, [key]: value }));
+      }
     },
     [key, setState]
   );
